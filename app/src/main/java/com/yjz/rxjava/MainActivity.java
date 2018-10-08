57f5e6da77788a28aa48716b62c6f168032de8bb
yangjiazhong <925873254@qq.com>
2018-10-08 20:22:47 +0800

    init project

---
+++ app/src/main/java/com/yjz/rxjava/MainActivity.java
@@ -0,0 +1,17 @@
+package com.yjz.rxjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author YJZ
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
